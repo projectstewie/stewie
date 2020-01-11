01d76ec754e3a5e5a671a4c7932f03e02b6b8918
Kristina Calumpong <kcalumpong@gmail.com>
2020-01-10 19:33:56 -0800

    Kristinas local changes for react

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavtabTop from "./components/NavtabTop";
+import NavtabBottom from "./components/NavtabBottom";
+import HeroImage from "./components/HeroImage";
+import './App.css';
+
+class App extends Component {
+
+  state = {
+   
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    console.log("Hey this works")
+    return (
+      // <Router>
+        <div>
+          <NavtabTop />
+          <NavtabBottom />
+          <HeroImage />
+
+          {/* <Switch>
+            <Route exact path="/" component={Signup} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/home" render={(props) => <Home {...props} handleLinkClick={this.handleLinkClick} />} />
+
+            <div className="grid">
+              <Route exact path="/employees" render={(props) => <Employees {...props} handleLinkClick={this.handleLinkClick} currentEmployeeId={this.state.currentEmployeeId} />} />
+              <Route path={"/employees/" + this.state.currentEmployeeId} render={(props) => <Info {...props} handleLinkClick={this.handleLinkClick} currentEmployeeId={this.state.currentEmployeeId} />} />
+            </div>
+
+          </Switch> */}
+        </div>
+      // </Router>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/NavtabBottom/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+const NavtabBottom = () => (
+ <div>
+     <div className="site-wrap">
+     <header className="site-navbar" role="banner">
+        <nav className="site-navigation text-right text-md-center" role="navigation">
+        <div className="container">
+          <ul className="site-menu js-clone-nav d-none d-md-block">
+            <li className="has-children active">
+              <a href="index.html">BUY</a>
+              <ul className="dropdown">
+                <li><a href="#">Clothing and Accessories</a></li>
+                <li><a href="#">Bath and Potty</a></li>
+                <li><a href="#">Bedding and Decor</a></li>
+                <li><a href="#">Toys and Learning</a></li>
+                <li><a href="#">Health and Safety</a></li>
+                <li><a href="#">Car Seats and Strollers</a></li>
+                <li><a href="#">Travel Accessories</a></li>
+                <li><a href="#">Bikes and Scooters</a></li>
+                <li><a href="#">Nursery and Kids Room</a></li>
+                <li><a href="#">Maternity and Nursery</a></li>
+              </ul>
+            </li>
+         
+            <li><a href="#">SELL</a></li>
+            <li><a href="#">CONTACT US</a></li>
+          </ul>
+        </div>
+      </nav>
+      </header>
+    </div>
+ </div>
+)
+
+export default NavtabBottom;
