b193abe8d86d1710e9c1ee71382aeab9fed81260
Kristina Calumpong <kcalumpong@gmail.com>
2020-01-24 18:07:38 -0800

    changes

--- src/components/ProductDescription/index.js
+++ src/components/ProductDescription/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./style.css";
-import NavtabBottom from "../NavtabBottom/";
-import NavtabTop from "../NavtabTop/";
 
 const ProductDescription = () => (
 
--- src/pages/Pdp.js
+++ src/pages/Pdp.js
@@ -2,13 +2,9 @@ import React, { Component } from "react";
 // import { Redirect } from 'react-router-dom';
 import NavtabTop from "../components/NavtabTop";
 import NavtabBottom from "../components/NavtabBottom";
-import Footer from "../components/Footer";
-import Listing from "../components/Listing";
-import Sort from "../components/Sort";
-import Categories from "../components/Categories";
-import Filter from "../components/Filter";
-import "../App.css";
 import ProductDescription from "../components/ProductDescription";
+import Footer from "../components/Footer";
+import "../App.css";
 
 class Pdp extends Component {
     // state = {
