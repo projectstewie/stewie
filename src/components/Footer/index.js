c53460f07f45aca20dc911092892cd3a5ae193b8
Daria Korbysheva <47726186+chuckthefrenchie@users.noreply.github.com>
2020-08-20 22:11:43 -0700

    updated Listing Component css and added props to Listing Page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -38,7 +38,7 @@ const Footer = () => (
           <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
             <h3 className="footer-heading mb-4">Promo</h3>
             <h4 href="#" className="block-6">
-              <img src="images/hero_1.jpg" alt="Image placeholder" className="img-fluid rounded mb-4"></img>
+              <img src={('/images/hero_1.jpg')} alt="Image placeholder" className="img-fluid rounded mb-4"></img>
               <h3 className="font-weight-light  mb-0">Finding Your Perfect Shoes</h3>
               <p>Promo from  January 15 &mdash; 25, 2020</p>
             </h4>
--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -6,17 +6,33 @@ import ListingComponent from "../components/Listing";
 import "../App.css";
 import "../css/style.css";
 import "../fonts/icomoon/style.css";
-
+import "../images/hero_1.jpg";
 
 class Listing extends Component {
 
-    constructor() {
-        super();
-    }
+    constructor(props) {
+        super(props);
+    
 
-    state = {
-        listing: []
-    }
+    this.state = {
+        listing: [
+        {
+            name: "Blue T-shirt",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, vitae, explicabo? Incidunt facere, natus soluta dolores iusto! Molestiae expedita veritatis nesciunt doloremque sint asperiores fuga voluptas, distinctio, aperiam, ratione dolore.",
+            color: "blue",
+            size: "S", 
+            price: "$15"
+        },
+        {
+            name: "Running shoes",
+            description: "Adidas Running Shoes",
+            color: "dark blue",
+            size: "8",
+            price: "$54"
+        }
+    ]
+    };
+}
 
     componentDidMount = () => {
         fetch('http://localhost:4000/listings/345234523')
