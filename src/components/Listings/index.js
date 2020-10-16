3e6b04b955543db392c46b36ef2fdbcbae2bf706
Kristina Calumpong <kcalumpong@gmail.com>
2020-10-16 00:46:40 -0700

    added listings route, mapped through listings, still need to map through the images and add image carousel

--- src/components/Listings/index.js
+++ src/components/Listings/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import "./style.css";
 
-const Listings = (props) => (
+const Listings = ({ listings }) => {
 
-  <div className="row mb-5">
-  
-      <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-        <div className="block-4 text-center border">
-          {/* <figure className="block-4-image"> */}
-          {/* <img src={props.images.itemId1} className='img-fluid'></img> */}
-          <img src='/images/blog_1.jpg' className='img-fluid'></img>
-            {/* <a href={"/listing/" + props.id}><img src={item.img} alt="Image placeholder" className="img-fluid"></img></a> */}
-          {/* </figure> */}
-          <div className="block-4-text p-4">
-            {/* <h3><a href={"/listing/123" + item.id}>{item.title}</a></h3> */}
-            <h3><a href={"/listing/123"}>{props.listings.heading}</a></h3>
-            {/* <p className="text-primary font-weight-bold">{props.listings.heading}</p> */}
-            <p className="mb-0">{props.listings.description}</p>
-            <p className="mb-0">{props.listings.price}</p>
+  // const { heading } = listings;
+
+  // let listingsItems = listings && listings.map(item =>
+  //   <div>
+  //     <p>{item.price}</p>
+  //   </div>
+  //   )
+
+  return (
+
+    <div className="row mb-5">
+      {listings && listings.map(item =>
+
+
+        <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+          <div className="block-4 text-center border">
+
+          
+            <img src='/images/blog_1.jpg' className='img-fluid'></img>
+      
+
+            <div className="block-4-text p-4">
+
+              <h3><a href={`/listing/${item.id}`}>{item.heading}</a></h3>
+
+              <p className="mb-0">{item.description}</p>
+              <p className="mb-0">{item.price}</p>
+            </div>
           </div>
         </div>
-      </div>
-  </div>
-)
+      )}
+    </div>
+  )
+}
+
 
 export default Listings;
--- src/pages/Listings.js
+++ src/pages/Listings.js
@@ -9,24 +9,18 @@ import "../App.css";
 
 class Listings extends Component {
 
-    state = {
-        listings: []
-    }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            listings: []
+        };
+    };
 
     componentDidMount = () => {
-        // This endpoint will change
-        fetch('http://localhost:4000/listings/123')
+        fetch('http://localhost:4000/listings')
             .then(res => res.json())
-            // .then(data => 
-            //     data.map(item => (
-            //         {
-            //             heading: item.heading,
-            //             description:item.description,
-            //             price:item.price,
-            //             selling_id:item.selling_id
-            //         }
-            //     )))
-            .then(item => this.setState({ listings: item }))
+            .then(allListings => this.setState({ listings: allListings }))
     }
 
     render() {
@@ -36,7 +30,7 @@ class Listings extends Component {
                 <NavtabBottom />
                 <Sort />
                 <Categories />
-                <ListingsComponent listings={this.state.listings}/>
+                <ListingsComponent listings={this.state.listings.users}/>
                 <Footer />
             </div>
         )
